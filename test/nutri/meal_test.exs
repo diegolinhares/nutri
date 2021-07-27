1bb665a64c31c5a92974c1ff69f4009b778cfb88
Diego Linhares <diegomlinhares@gmail.com>
2021-07-27 15:20:00 -0300

    Add tests to meal changeset

---
+++ test/nutri/meal_test.exs
@@ -0,0 +1,65 @@
+defmodule Nutri.MealTest do
+  use Nutri.DataCase, async: true
+
+  alias Nutri.Meal
+  alias Ecto.Changeset
+
+  import Nutri.Factory
+
+  describe "changeset/2" do
+    test "when all params are valid, returns a valid changeset" do
+      # Arrange
+      params = build(:meal_params)
+
+      # Act
+      response = Meal.changeset(params)
+
+      # Assert
+      assert %Changeset{
+               changes: %{
+                 calories: 1.0e3,
+                 date: ~U[2021-05-12 11:00:00Z],
+                 description: "Arroz Branco"
+               },
+               errors: [],
+               valid?: true
+             } = response
+    end
+
+    test "when updating a changeset, returns a valid changeset with the given changes" do
+      # Arrange
+      params = build(:meal_params)
+
+      update_params = %{"description" => "Feijão"}
+
+      # Act
+      response =
+        params
+        |> Meal.changeset()
+        |> Meal.update_changeset(update_params)
+
+      # Assert
+      assert %Changeset{
+               changes: %{description: "Feijão"}
+             } = response
+    end
+
+    test "when there are some error, returns an invalid changeset" do
+      # Arrange
+      params = build(:meal_params, %{"calories" => 0})
+
+      # Act
+      response = Meal.changeset(params)
+
+      # Assert
+      assert %Changeset{
+               errors: [
+                 calories:
+                   {"must be greater than %{number}",
+                    [validation: :number, kind: :greater_than, number: 0]}
+               ],
+               valid?: false
+             } = response
+    end
+  end
+end
