aaf5aa474878f046623b975e2dd34a9e6bacd966
Diego Linhares <diegomlinhares@gmail.com>
2021-07-27 15:00:02 -0300

    Add meals controller create errors test

--- test/nutri_web/controllers/meals_controller_test.exs
+++ test/nutri_web/controllers/meals_controller_test.exs
@@ -23,5 +23,24 @@ defmodule NutriWeb.MealsControllerTest do
       assert %{"meal" =>
                 %{"calories" => 1.0e3, "date" => "2021-05-12T00:00:00Z", "description" => "Arroz Branco", "id" => _id}} = response
     end
+
+    test "when params are invalid, returns an error", %{conn: conn} do
+      # Arrange
+      params =
+        %{
+          "description" => "Arroz Branco",
+          "calories" => "0",
+          "date" => "2021-05-12 00:00:00.000001"
+        }
+
+      # Act
+      response =
+        conn
+        |> post(Routes.meals_path(conn, :create, params))
+        |> json_response(:bad_request)
+
+      # Assert
+      assert %{"message" => %{"calories" => ["must be greater than %{number}"]}} = response
+    end
   end
 end
