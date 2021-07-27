987672c8273d0a97af1a0ae88e6c39f5378079d5
Diego Linhares <diegomlinhares@gmail.com>
2021-07-27 01:43:01 -0300

    Add meal schema

---
+++ lib/nutri/meal.ex
@@ -0,0 +1,19 @@
+defmodule Nutri.Meal do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @required_fields [:description, :calories, :date]
+
+  schema "meals" do
+    field :description, :string
+    field :calories, :float
+    field :date, :utc_datetime
+  end
+
+  def changeset(params) do
+    %__MODULE__{}
+    |> cast(params, @required_fields)
+    |> validate_required(@required_fields)
+    |> validate_number(:calories, greater_than: 0)
+  end
+end
