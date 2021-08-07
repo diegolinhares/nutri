0c3f63de0439f92484c94e2260eaceefbafa61c1
Diego Linhares <diegomlinhares@gmail.com>
2021-08-07 02:21:10 -0300

    Create users table

---
+++ priv/repo/migrations/20210807050901_create_users_table.exs
@@ -0,0 +1,14 @@
+defmodule Nutri.Repo.Migrations.CreateUsersTable do
+  use Ecto.Migration
+
+  def change do
+    create table(:users) do
+      add :name, :string
+      add :email, :string
+      add :cpf, :string
+    end
+
+    create unique_index(:users, [:email])
+    create unique_index(:users, [:cpf])
+  end
+end
